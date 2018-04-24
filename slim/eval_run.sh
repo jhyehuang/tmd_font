5824019e4432e7876ce142ce3090c08c5fd1efc5
jhyehuang <huangzhijie3918@sina.com>
2018-04-24 19:40:00 -0400

    a

---
+++ slim/eval_run.sh
@@ -0,0 +1,7 @@
+python eval_image_classifier.py \
+  --checkpoint_path=/home/zhijie.huang/github/data/TMD/train_set \
+  --eval_dir=/home/zhijie.huang/github/data/TMD/train_set \
+  --dataset_name=chinese_font \
+  --dataset_split_name=validation \
+  --dataset_dir=/home/zhijie.huang/github/data/TMD \
+  --model_name=densenet
--- slim/run.sh
+++ slim/run.sh
@@ -5,9 +5,9 @@ python -u train_image_classifier.py \
     --dataset_name=chinese_font \
     --dataset_dir=/home/zhijie.huang/github/data/TMD \
     --model_name=densenet \
-    --checkpoint_path=/home/zhijie.huang/github/data/chinese_font_ckpt/model.ckpt-1032 \
+    --checkpoint_path=/home/zhijie.huang/github/data/TMD/train_set/model.ckpt-14448 \
     --train_dir=/home/zhijie.huang/github/data/TMD/train_set \
-    --learning_rate=0.1 \
+    --learning_rate=0.001 \
     --optimizer=rmsprop \
     --ignore_missing_vars=True \
     --batch_size=32
